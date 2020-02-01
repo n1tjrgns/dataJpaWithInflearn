8cb890f5e50a9cdd4ee3fb6d29fb46215c0f348c
n1tjrgns <n2tjrgns@gmail.com>
2020-02-01 15:17:11 +0900

    2. 엔티티 타입 맵핑

--- src/main/java/com/example/datajpa/Account.java
+++ src/main/java/com/example/datajpa/Account.java
@@ -1,8 +1,7 @@
 package com.example.datajpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Account {
@@ -11,9 +10,17 @@ public class Account {
     @GeneratedValue
     private Long id;
 
+    //모든 멤버변수에는 @Column이 생략되어있음
     private String username;
 
     private String password;
+
+    //요즘엔 LocalDateTime.now 같은걸 사용함
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    @Transient //@Transient 컬럼으로 매핑을 안해줌
+    private String no;
 
     public Long getId() {
         return id;
